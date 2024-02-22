2f1e5897a240551210ecc436c92e8103500e5b5f
Matheus <mp_siqueira@hotmail.com>
2024-02-22 15:02:26 -0300

    [GhibliFilms] Hover Film Info

--- components/atoms/FilmComponent.vue
+++ components/atoms/FilmComponent.vue
@@ -1,14 +1,17 @@
 <template>
   <NuxtLink :to="`/films/${Film.id}`" >
-    <div>
-      <h3>{{ Film.title }}</h3>
-      <img :src="Film.image" :alt="Film.title" class="film-item"/>
-      <div class="film-details">
-        <div>{{ Film.release_date }}</div>
-        <div>{{ Film.director }}</div>
-        <div>{{ Film.producer }}</div>
-        <div>{{ Film.description }}</div>
-        <div>{{ Film.rt_score }}</div>
+    <div class="film-wrapper">
+      <h4>{{ Film.title }}</h4>
+      <div class="film-item-wrapper">
+        <img :src="Film.image" :alt="Film.title" class="film-item"/>
+        <div class="film-details">
+          <div>{{ Film.description }}</div>
+          <br>
+          <div><i>{{ Film.release_date }}</i></div>
+          <div><b>{{ Film.director }}</b></div>
+          <div><b>{{ Film.producer }}</b></div>
+          <div>Rating: {{ Film.rt_score }}</div>
+        </div>
       </div>
     </div>
   </NuxtLink>
@@ -24,16 +27,42 @@ const props = defineProps ({
 </script>
 
 <style lang="scss" scoped>
+.film-wrapper {
+  position: relative;
+  margin-bottom: 20px; /* Adjust as needed */
+}
+
+.film-item-wrapper {
+  position: relative;
+}
+
 .film-item {
   width: 150px;
   height: 200px;
-  object-fit: cover; /* Ensures image fills container while maintaining aspect ratio */
-  .film-details {
-    opacity: 0;
-    transition: opacity 0.3s ease-in-out; /* Adjust transition duration as needed */
-  }
-  &:hover .film-details {
-    opacity: 1;
-  }
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.8);
+  object-fit: cover;
+  position: relative;
+}
+
+.film-details {
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.3s ease-in-out, visibility 0s linear 0.3s;
+  position: absolute;
+  top: 70%;
+  left: 0;
+  right: 0;
+  background-color: rgba($color: #292929, $alpha: 1.0); 
+  color: rgba($color: #ffffff, $alpha: 1.0);
+  width: auto; /* Adjust width as needed */
+  height: auto; /* Adjust height as needed */
+  padding: 10px; 
+  border-radius: 7px; /* Add border-radius as needed */
+  z-index: 1; /* Ensure the details appear above the image */
+}
+
+.film-item-wrapper:hover .film-details {
+  opacity: 1;
+  visibility: visible;
 }
 </style>
--- components/bosons/ConteudoContainer.vue
+++ components/bosons/ConteudoContainer.vue
@@ -9,7 +9,7 @@
   margin: 0 auto;
   padding-top: 1rem;
   max-height: 1800px;
-  max-width: 1200px;
+  max-width: 1400px;
   //background-color: rgba($color: #ff1b1b, $alpha: 0.2);
 }
 </style>
